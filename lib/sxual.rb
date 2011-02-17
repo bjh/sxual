a866d20d873641f89606568e72e49391ae10f673
bjh <brodie.hanbuch@gmail.com>
2011-02-16 21:04:29 -0500

    creating bin

--- lib/sxual.rb
+++ lib/sxual.rb
@@ -1,3 +1,6 @@
+require 'nokogiri'
+require 'trollop'
+
 module Sxual
   # Your code goes here...
 end
---
+++ lib/sxual/args.rb
@@ -0,0 +1,12 @@
+module Sxual
+
+  opts = Trollop::options do
+    opt :monkey, "Use monkey mode"                     # flag --monkey, default false
+    opt :goat, "Use goat mode", :default => true       # flag --goat, default true
+    opt :num_limbs, "Number of limbs", :default => 4   # integer --num-limbs <i>, default to 4
+    opt :num_thumbs, "Number of thumbs", :type => :int # integer --num-thumbs <i>, default nil
+  end
+
+  p opts # a hash: { :monkey => false, :goat => true, :num_limbs => 4, :num_thumbs => nil }
+
+end
