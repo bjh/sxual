6bced8f4c5373856ef9eb33f771a9284cdf69766
bjh <brodie.hanbuch@gmail.com>
2011-02-23 19:23:09 -0500

    tweaking

--- lib/sxual.rb
+++ lib/sxual.rb
@@ -4,5 +4,13 @@ require 'trollop'
 require 'sxual/args'
 
 module Sxual
-  
+  class Application
+    def run
+      args = Args.new
+      args.parse
+
+      # puts args.set? :monkey
+      # puts args.set? :goat
+    end
+  end
 end
