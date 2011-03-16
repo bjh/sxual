c9f444d457392bcccdaac7a4d8d2684e22f11f2b
bjh <brodie.hanbuch@gmail.com>
2011-03-16 19:34:37 -0400

    blam

--- lib/sxual/walkers/base_walker.rb
+++ lib/sxual/walkers/base_walker.rb
@@ -2,7 +2,8 @@ module Sxual
   module Walkers
     class BaseWalker
 
-      protected def walk
+      protected
+      def walk
         throw "please override me in a sxual fashion"
       end
     end
