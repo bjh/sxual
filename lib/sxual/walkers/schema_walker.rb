80016262ec3d36c0ad5e4efd85ded4f787e27622
bjh <brodie.hanbuch@gmail.com>
2011-03-16 19:48:48 -0400

    blam

--- lib/sxual/walkers/schema_walker.rb
+++ lib/sxual/walkers/schema_walker.rb
@@ -11,7 +11,7 @@ module Sxual
 
           table[:fields].each do |field|
             print "\tt.#{field[:type]}, :#{field[:name]}"
-            #print ", :default => #{field[:default]}" if field[:default]
+            print ", :default => #{field[:default]}" if field[:default]
             print ", :null => false" if field[:not_null]
             print "\n"
           end
