da4d32d5670a8e04a95f41721ec65cca6720f2a0
bjh <brodie.hanbuch@gmail.com>
2011-03-16 20:04:18 -0400

    blam

--- lib/sxual/walkers/schema_walker.rb
+++ lib/sxual/walkers/schema_walker.rb
@@ -6,22 +6,19 @@ module Sxual
       end
 
       def walk
-        @data.each do |table|
-          
-          
+        @data.each do |table|          
           table_has_id = false
           
           fields = table[:fields].map do |field|
-                        
+            
             if field[:name].downcase == 'id'
               table_has_id = true
-            else
-              
-              s = "\tt.#{field[:type]}, :#{field[:name]}"
-              s << ", :default => #{field[:default]}" if field[:default]
-              s << ", :null => false" if field[:not_null]
-              return s
+              continue
             end
+              
+            s = "\tt.#{field[:type]}, :#{field[:name]}"
+            s << ", :default => #{field[:default]}" if field[:default]
+            s << ", :null => false" if field[:not_null]
           end
           
           puts "create_table :#{table[:name]}, do |t|"
